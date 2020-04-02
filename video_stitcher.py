346588da669dba9e165cdac6a9053754e105461e
novandev <novanadams35@gmail.com>
2020-04-01 18:17:13 -0700

    Starting celerywork

--- video_stitcher.py
+++ video_stitcher.py
@@ -3,9 +3,15 @@ from moviepy.editor import *
 from glob import glob
 
 def stitch_all_gifs(videos_path):
-    file_dir = glob(f'{videos_path}/*.gif')
+    files_in_dir = sorted(glob(f'{videos_path}/*.gif'))
+    stitched_video = []
+    for video in files_in_dir:
+        stitched_video.append(VideoFileClip(video))
 
-    print(sorted(file_dir))
+    full_composite_video = concatenate_videoclips(stitched_video, method='compose')
+
+    full_composite_video.write_videofile("stitched_video.mp4")
+
 
 
 # final_clip = concatenate_videoclips([clip1,clip2,clip3])
